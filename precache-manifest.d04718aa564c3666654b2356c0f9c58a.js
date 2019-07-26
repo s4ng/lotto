0670457a0b98d92d1e1c94b2d5f4924a3a609d7f
s4ng <zalcls951201@gmail.com>
2019-07-26 23:30:46 +0900

    Updates

---
+++ precache-manifest.d04718aa564c3666654b2356c0f9c58a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c9c4012f27249d53f800b9e734532d",
+    "url": "/lotto/index.html"
+  },
+  {
+    "revision": "5dd8f4a6b1e4dfdfc2db",
+    "url": "/lotto/static/css/main.d244a3d1.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/lotto/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "5dd8f4a6b1e4dfdfc2db",
+    "url": "/lotto/static/js/main.bc58be5c.chunk.js"
+  },
+  {
+    "revision": "826cbfe86c78cf2637c3",
+    "url": "/lotto/static/js/runtime~main.10e76465.js"
+  }
+]);
